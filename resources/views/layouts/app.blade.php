9756a5ea3d1bcadaa5868f3cd1652601bdb7b4b9
Buddhika <lsnt4@users.noreply.github.com>
2019-11-30 17:31:56 +0530

    CRUD functions finished

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,9 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="bg-dark">
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -49,6 +49,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('notes.create') }}">New Note</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('notes', 'NoteController@index')->name('notes.index');
+Route::get('notes/create', 'NoteController@create')->name('notes.create');
+Route::post('notes', 'NoteController@store');
+Route::get('notes/{note}/edit', 'NoteController@edit');
+Route::put('notes/{note}', 'NoteController@update');
+Route::delete('notes/{note}', 'NoteController@destroy');
